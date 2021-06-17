c12068dcf1c67af2b80a9f67d8edb4f1c6b4a5e5
mramdan <mramdan480@gmail.com>
2021-06-17 17:40:39 +0700

    menambahkan halaman utama

--- index.php
+++ index.php
@@ -1,30 +1,265 @@
-<?php
-include "view/_header.php";
-include "view/_menu.php";
-?>
-      <main class="app-main">
-        <!-- .wrapper -->
-        <div class="wrapper">
-          <!-- .empty-state -->
-          <div id="notfound-state" class="empty-state">
-            <!-- .empty-state-container -->
-            <div class="empty-state-container">
-              <div class="state-figure">
-                <img class="img-fluid" src="../assets/images/illustration/img-6.svg" alt="" style="max-width: 300px">
-              </div>
-              <h3 class="state-header"> No Content, Yet. </h3>
-              <p class="state-description lead text-muted"> Use the button below to add your awesomething, aperiam ex veniam suscipit porro ab saepe nobis odio. </p>
-              <div class="state-action">
-                <a href="#" class="btn btn-primary">Create New</a>
-              </div>
-              <div class="btn btn-light fileinput-button">
-                <i class="oi oi-cloud-upload mr-1"></i> <span>Import File</span> <input id="fileupload-btn" type="file" name="file">
-              </div>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title> Penerimaan Karyawan Baru PT. STARPIA</title>
+  <link href="https://fonts.googleapis.com/css?family=Fira+Sans:400,500,600" rel="stylesheet">
+  <link rel="stylesheet" href="assets/vendor/aos/aos.css">
+  <link rel="stylesheet" href="assets/stylesheets/theme.min.css" data-skin="default">
+  <link rel="stylesheet" href="assets/stylesheets/theme-dark.min.css" data-skin="dark">
+  <link rel="stylesheet" href="assets/stylesheets/custom.css">
+  <script>
+    var skin = localStorage.getItem('skin') || 'default';
+    var disabledSkinStylesheet = document.querySelector('link[data-skin]:not([data-skin="' + skin + '"])');
+    disabledSkinStylesheet.setAttribute('rel', '');
+    disabledSkinStylesheet.setAttribute('disabled', true);
+    document.querySelector('html').classList.add('loading');
+  </script>
+</head>
+
+<body>
+  <!-- .app -->
+  <main class="app app-site">
+    <!--[if lt IE 10]>
+      <div class="page-message" role="alert">You are using an <strong>outdated</strong> browser. Please <a class="alert-link" href="http://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</div>
+      <![endif]-->
+    <!-- site header -->
+    <!-- .navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light py-4" data-aos="fade-in">
+      <div class="container">
+        <button class="hamburger hamburger-squeeze hamburger-light d-flex d-lg-none" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation"><span class="hamburger-box"><span class="hamburger-inner"></span></span></button> <!-- /.hamburger -->
+        <a class="navbar-brand ml-auto mr-0" href="#"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="28" viewbox="0 0 351 100">
+            <defs>
+              <path id="a" d="M156.538 45.644v1.04a6.347 6.347 0 0 1-1.847 3.98L127.708 77.67a6.338 6.338 0 0 1-3.862 1.839h-1.272a6.34 6.34 0 0 1-3.862-1.839L91.728 50.664a6.353 6.353 0 0 1 0-9l9.11-9.117-2.136-2.138a3.171 3.171 0 0 0-4.498 0L80.711 43.913a3.177 3.177 0 0 0-.043 4.453l-.002.003.048.047 24.733 24.754-4.497 4.5a6.339 6.339 0 0 1-3.863 1.84h-1.27a6.337 6.337 0 0 1-3.863-1.84L64.971 50.665a6.353 6.353 0 0 1 0-9l26.983-27.008a6.336 6.336 0 0 1 4.498-1.869c1.626 0 3.252.622 4.498 1.87l26.986 27.006a6.353 6.353 0 0 1 0 9l-9.11 9.117 2.136 2.138a3.171 3.171 0 0 0 4.498 0l13.49-13.504a3.177 3.177 0 0 0 .046-4.453l.002-.002-.047-.048-24.737-24.754 4.498-4.5a6.344 6.344 0 0 1 8.996 0l26.983 27.006a6.347 6.347 0 0 1 1.847 3.98zm-46.707-4.095l-2.362 2.364a3.178 3.178 0 0 0 0 4.501l2.362 2.364 2.361-2.364a3.178 3.178 0 0 0 0-4.501l-2.361-2.364z"></path>
+            </defs>
+            <g fill="none" fill-rule="evenodd">
+              <path fill="currentColor" fill-rule="nonzero" d="M39.252 80.385c-13.817 0-21.06-8.915-21.06-22.955V13.862H.81V.936h33.762V58.1c0 6.797 4.346 9.026 9.026 9.026 2.563 0 5.237-.446 8.58-1.783l3.677 12.034c-5.794 1.894-9.694 3.009-16.603 3.009zM164.213 99.55V23.78h13.372l1.225 5.571h.335c4.457-4.011 10.585-6.908 16.491-6.908 13.817 0 22.174 11.031 22.174 28.08 0 18.943-11.588 29.863-23.957 29.863-4.903 0-9.694-2.117-13.594-6.017h-.446l.78 9.025V99.55h-16.38zm25.852-32.537c6.128 0 10.92-4.903 10.92-16.268 0-9.917-3.232-14.932-10.14-14.932-3.566 0-6.797 1.56-10.252 5.126v22.397c3.12 2.674 6.686 3.677 9.472 3.677zm69.643 13.372c-17.272 0-30.643-10.586-30.643-28.972 0-18.163 13.928-28.971 28.748-28.971 17.049 0 26.075 11.477 26.075 26.52 0 3.008-.558 6.017-.78 7.354h-37.663c1.56 8.023 7.465 11.589 16.491 11.589 5.014 0 9.36-1.337 14.263-3.9l5.46 9.917c-6.351 4.011-14.597 6.463-21.951 6.463zm-1.338-45.463c-6.462 0-11.031 3.454-12.702 10.363h23.622c-.78-6.797-4.568-10.363-10.92-10.363zm44.238 44.126V23.779h13.371l1.337 12.034h.334c5.46-9.025 13.595-13.371 22.398-13.371 4.902 0 7.465.78 10.697 2.228l-3.343 13.706c-3.454-1.003-5.683-1.56-9.806-1.56-6.797 0-13.928 3.566-18.608 13.483v28.749h-16.38z"></path>
+              <use class="fill-primary" xlink:href="#a"></use>
+            </g>
+          </svg></a>
+        <a class="navbar-btn btn btn-subtle-primary ml-auto order-lg-2" href="#">Pengumuman</a>
+        <div class="navbar-collapse collapse" id="navbarTogglerDemo01">
+          <ul class="navbar-nav mx-auto">
+            <li class="nav-item mr-lg-3 active">
+              <a class="nav-link py-2" href="#">Home</a>
+            </li>
+            <li class="nav-item mr-lg-3">
+              <a class="nav-link py-2" href="#">Lowongan Kerja</a>
+            </li>
+            <li class="nav-item mr-lg-3">
+              <a class="nav-link py-2" href="#">Pengumuman</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <section class="py-5">
+      <div class="container container-fluid-xl">
+        <div class="row align-items-center">
+          <div class="col-12 col-md-6 order-md-2" data-aos="fade-left">
+            <img class="img-fluid img-float-md-6 mb-5 mb-md-0" src="assets/images/illustration/launch.svg" alt="">
+          </div>
+
+          <div class="col-12 col-md-6 order-md-1" data-aos="fade-in">
+            <div class="col-fix pl-xl-3 ml-auto text-center text-sm-left">
+              <h1 class="display-4 enable-responsive-font-size mb-4"> Thanks for <strong>give me a try</strong>. I'm the <strong>theme</strong> for your <strong>project</strong>! </h1>
+              <p class="lead text-muted mb-5"> Build rich and beautiful experiences for your project. Increase your productivity and grow your business. </p><a href="index.html" class="btn btn-lg btn-primary d-block d-sm-inline-block mr-sm-2 my-3" data-aos="zoom-in" data-aos-delay="200">Let's Try <i class="fa fa-angle-right ml-2"></i></a> <a href="/docs" class="btn btn-lg btn-subtle-primary d-block d-sm-inline-block my-3" target="_blank" data-aos="zoom-in" data-aos-delay="300">Documentation</a>
             </div>
           </div>
         </div>
-      </main>
+      </div>
+    </section>
 
-<?php
-include "view/_footer.php";
-?>
+    <!-- sections title -->
+    <section class="py-5">
+      <!-- .container -->
+      <div class="container">
+        <!-- .row -->
+        <div class="row">
+          <!-- .col -->
+          <div class="col-12 col-md-8 offset-md-2 text-center">
+            <h2> With You From the Ground Up </h2>
+            <p class="lead text-muted"> Architected as an extension of Bootstrap, built to reach your goals. </p>
+          </div><!-- /.col -->
+        </div>
+      </div><!-- /.container -->
+    </section><!-- /sections title -->
+    <!-- feature -->
+    <section class="py-5">
+      <!-- .container -->
+      <div class="container">
+        <!-- .row -->
+        <div class="row align-items-center">
+          <!-- .col-md-6 -->
+          <div class="col-12 col-md-6" data-aos="fade-right">
+            <img class="img-fluid mb-4 mb-md-0" src="assets/images/illustration/solution.svg" alt="">
+          </div>
+          <!-- .col-md-6 -->
+          <div class="col-12 col-md-6 text-center text-sm-left">
+            <h3 class="mb-4"> Increase your productivity </h3>
+            <p class="text-muted font-size-lg mb-4"> Create stunning UI with tons of our components. Our build system allows you to save a lot of time as well. </p><a href="#" class="btn btn-subtle-primary">Browse all components</a>
+          </div>
+        </div>
+      </div><!-- /.container -->
+    </section>
+    <section class="pt-5">
+      <!-- .container -->
+      <div class="container">
+        <!-- .row -->
+        <div class="row align-items-center">
+          <!-- .col-md-6 -->
+          <div class="col-12 col-md-6 order-md-2" data-aos="fade-left">
+            <img class="img-fluid mb-4 mb-md-0" src="assets/images/illustration/testimony.svg" alt="">
+          </div>
+          <!-- .col-md-6 -->
+          <div class="col-12 col-md-6 order-md-1 text-center text-sm-left">
+            <h3 class="mb-4"> Grow your business </h3>
+            <p class="text-muted font-size-lg mb-4"> With a user-friendly, simple and responsive design, you can easily attract your prospective clients. Don't worry! our team will always be behind you if needed. </p><a href="#" class="btn btn-subtle-primary">Explore details</a>
+          </div>
+        </div>
+      </div><!-- /.container -->
+    </section><!-- /feature -->
+    <!-- divider -->
+    <section class="position-relative">
+      <!-- wave2.svg -->
+      <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="192" viewbox="0 0 1440 240" preserveaspectratio="none">
+        <path class="fill-light" fill-rule="evenodd" d="M0 240V0c19.985 5.919 41.14 11.008 63.964 14.89 40.293 6.855 82.585 9.106 125.566 9.106 74.151 0 150.382-6.697 222.166-8.012 13.766-.252 27.51-.39 41.21-.39 99.76 0 197.087 7.326 282.907 31.263C827.843 72.527 860.3 117.25 906.926 157.2c43.505 37.277 115.38 51.186 208.485 53.076 7.584.153 15.156.224 22.714.224 40.887 0 81.402-2.062 121.914-4.125 40.512-2.062 81.027-4.125 121.902-4.125 1.01 0 2.019.002 3.03.004 16.208.042 34.959.792 55.029 2.234V240H0z"></path>
+      </svg>
+    </section><!-- /divider -->
+    <!-- feature -->
+    <section class="position-relative pb-5 bg-light">
+      <!-- .sticker -->
+      <div class="sticker">
+        <div class="sticker-item sticker-top-right sticker-soften">
+          <img src="assets/images/decoration/cubes.svg" alt="" data-aos="zoom-in">
+        </div>
+        <div class="sticker-item sticker-bottom-left sticker-soften scale-150">
+          <img src="assets/images/decoration/cubes.svg" alt="" data-aos="zoom-in">
+        </div>
+      </div><!-- /.sticker -->
+      <!-- .container -->
+      <div class="container position-relative">
+        <h2 class="text-center text-sm-left"> Get more benefits </h2>
+        <p class="lead text-muted text-center text-sm-left mb-5"> Scaffold your theme and start developing quickly. </p><!-- .card-deck -->
+        <div class="card-deck-lg">
+          <!-- .card -->
+          <div class="card shadow" data-aos="fade-up" data-aos-delay="0">
+            <!-- .card-body -->
+            <div class="card-body p-4">
+              <div class="d-sm-flex align-items-start text-center text-sm-left">
+                <img src="assets/images/illustration/rocket.svg" class="mr-sm-4 mb-3 mb-sm-0" alt="" width="72">
+                <div class="flex-fill">
+                  <h5 class="mt-0"> Ready-to-go </h5>
+                  <p class="text-muted font-size-lg"> Provides a tons of useful Components and Pages that allow you to focus on your application logic rather than re-touching pixels. </p>
+                </div>
+              </div>
+            </div><!-- /.card-body -->
+          </div><!-- /.card -->
+          <!-- .card -->
+          <div class="card shadow" data-aos="fade-up" data-aos-delay="100">
+            <!-- .card-body -->
+            <div class="card-body p-4">
+              <div class="d-sm-flex align-items-start text-center text-sm-left">
+                <img src="assets/images/illustration/setting.svg" class="mr-sm-4 mb-3 mb-sm-0" alt="" width="72">
+                <div class="flex-fill">
+                  <h5 class="mt-0"> Highly functionality </h5>
+                  <p class="text-muted font-size-lg"> Looper is not 1001 in 1 shit theme, there's not an obscene amount of wasted space, and we are carefully in adding useful features without going overboard. </p>
+                </div>
+              </div>
+            </div><!-- /.card-body -->
+          </div><!-- /.card -->
+        </div><!-- /.card-deck -->
+        <!-- .card-deck -->
+        <div class="card-deck-lg">
+          <!-- .card -->
+          <div class="card shadow" data-aos="fade-up" data-aos-delay="200">
+            <!-- .card-body -->
+            <div class="card-body p-4">
+              <div class="d-sm-flex align-items-start text-center text-sm-left">
+                <img src="assets/images/illustration/brain.svg" class="mr-sm-4 mb-3 mb-sm-0" alt="" width="72">
+                <div class="flex-fill">
+                  <h5 class="mt-0"> Boost your creativity </h5>
+                  <p class="text-muted font-size-lg"> The design is clean, simple, modern, and essential, with Lots of refined graphics and tons of 3rd party support. </p>
+                </div>
+              </div>
+            </div><!-- /.card-body -->
+          </div><!-- /.card -->
+          <!-- .card -->
+          <div class="card shadow" data-aos="fade-up" data-aos-delay="300">
+            <!-- .card-body -->
+            <div class="card-body p-4">
+              <div class="d-sm-flex align-items-start text-center text-sm-left">
+                <img src="assets/images/illustration/horse.svg" class="mr-sm-4 mb-3 mb-sm-0" alt="" width="72">
+                <div class="flex-fill">
+                  <h5 class="mt-0"> Simple workflow </h5>
+                  <p class="text-muted font-size-lg"> We make sure you can make a magic by running just two command. It was very easy, extremely powerful, and modern. </p>
+                </div>
+              </div>
+            </div><!-- /.card-body -->
+          </div><!-- /.card -->
+        </div><!-- /.card-deck -->
+        <!-- .card-deck -->
+        <div class="card-deck-lg">
+          <!-- .card -->
+          <div class="card shadow" data-aos="fade-up" data-aos-delay="400">
+            <!-- .card-body -->
+            <div class="card-body p-4">
+              <div class="d-sm-flex align-items-start text-center text-sm-left">
+                <img src="assets/images/illustration/savings.svg" class="mr-sm-4 mb-3 mb-sm-0" alt="" width="72">
+                <div class="flex-fill">
+                  <h5 class="mt-0"> One time payment </h5>
+                  <p class="text-muted font-size-lg"> After you make a purchase you will get a free update forever. We will make Looper better and better again for you. </p>
+                </div>
+              </div>
+            </div><!-- /.card-body -->
+          </div><!-- /.card -->
+          <!-- .card -->
+          <div class="card shadow" data-aos="fade-up" data-aos-delay="500">
+            <!-- .card-body -->
+            <div class="card-body p-4">
+              <div class="d-sm-flex align-items-start text-center text-sm-left">
+                <img src="assets/images/illustration/document.svg" class="mr-sm-4 mb-3 mb-sm-0" alt="" width="72">
+                <div class="flex-fill">
+                  <h5 class="mt-0"> Well Documented </h5>
+                  <p class="text-muted font-size-lg"> Each component is well explained with example, code block and best practices. We also provide extensive information about customizing Looper. </p>
+                </div>
+              </div>
+            </div><!-- /.card-body -->
+          </div><!-- /.card -->
+        </div><!-- /.card-deck -->
+      </div><!-- /.container -->
+    </section>
+
+    <!-- footer -->
+    <section class="py-5 bg-black text-white">
+      <!-- .container -->
+      <div class="container">
+        <p class="text-muted text-center mt-5"> © 2020 Looper, Inc. All rights reserved. </p>
+      </div><!-- /.container -->
+    </section><!-- /footer -->
+  </main><!-- /.app -->
+  <!-- BEGIN BASE JS -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/popper.js/umd/popper.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script> <!-- END BASE JS -->
+  <!-- BEGIN PLUGINS JS -->
+  <script src="assets/vendor/aos/aos.js"></script> <!-- END PLUGINS JS -->
+  <!-- BEGIN THEME JS -->
+  <script src="assets/javascript/theme.min.js"></script>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-116692175-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'UA-116692175-1');
+  </script>
+</body>
+
+</html>
